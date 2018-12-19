78c0ca2496e286160b5f6d0e052c944eeeb08179
Roshan Sharma <rsn.sbdi@gmail.com>
2018-12-19 11:07:20 +0545

    Adding Base

---
+++ app/src/main/java/com/example/roshan/nepxchange/Base/BaseDailogFragment.java
@@ -0,0 +1,11 @@
+package com.example.roshan.nepxchange.Base;
+
+import android.support.v4.app.DialogFragment;
+
+public class BaseDailogFragment extends DialogFragment {
+
+//    public DialogFragmentComponent getDiaFragmentComponent(){
+//        Injector.initialiseDialogFragmentComponent(this);
+//        return Injector.getDialogFragmentComponent();
+//    }
+}
