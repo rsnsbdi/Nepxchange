c534b39053e53422f40399a5064496f9d1d06468
Roshan Sharma <rsn.sbdi@gmail.com>
2018-12-23 15:53:33 +0545

    Splash Activity Added

---
+++ app/src/main/java/com/example/roshan/nepxchange/UI/Splash/Splash.java
@@ -0,0 +1,14 @@
+package com.example.roshan.nepxchange.UI.Splash;
+
+import android.os.Bundle;
+
+import com.example.roshan.nepxchange.Base.BaseActivity;
+
+public class Splash extends BaseActivity implements SplashView {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
